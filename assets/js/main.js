e54df819131222f1634a33bd93b0c04b3815bb07
Eli Brumley <elibrumley@gmail.com>
2025-08-21 19:08:12 -0500

    utility trailer feature carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -422,10 +422,10 @@
 
   // counterUp
 
-  $(".counter").counterUp({
-    delay: 10,
-    time: 1000
-  });
+  // $(".counter").counterUp({
+  //   delay: 10,
+  //   time: 1000
+  // });
 
   // testimonial
 
@@ -494,6 +494,23 @@
       1000: {
         items: 6
       }
+    }
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
     }
   });
 
